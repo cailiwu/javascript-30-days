9012d20bbed38c27a9f87c737b25b5f388b4a21c
ShellyWu <shelly.wu@kangdainfo.com>
2020-08-07 09:10:25 +0800

    Added Drum-Kit

---
+++ 01-Drum-Kit/index.js
@@ -0,0 +1,30 @@
+function playSound (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  e.target.classList.remove('playing');
+}
+
+
+
+
+
+
+
+
+
+const keys = Array.from(document.querySelectorAll('.key')); // DOM is array-like
+
+keys.forEach((key)=> {
+  key.addEventListener('transitionend', removeTransition)
+});
+
+window.addEventListener('keydown', playSound);
+
